6304abd8ca13c86548f4f91935329ffdb9538db2
hashito <hasitozzz@gmail.com>
2021-08-28 21:50:37 +0900

    a

--- selenium/main.py
+++ selenium/main.py
@@ -1,5 +1,10 @@
-import chromedriver_binary # nopa
+#import chromedriver_binary # nopa
+from get_chrome_driver import GetChromeDriver
 from selenium import webdriver
+
+get_driver = GetChromeDriver()
+get_driver.install()
+
 def driver_init():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
