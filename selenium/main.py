83f0441ccbfbe3df83c807bea71ac79156378b69
hashito <hasitozzz@gmail.com>
2021-09-01 21:26:19 +0900

    a

--- selenium/main.py
+++ selenium/main.py
@@ -1,4 +1,3 @@
-#import chromedriver_binary # nopa
 from get_chrome_driver import GetChromeDriver
 from selenium import webdriver
 
@@ -8,13 +7,10 @@ get_driver.install()
 def driver_init():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    print('connectiong to remote browser...')
     return webdriver.Chrome(options=options)
 
 driver = driver_init()
 driver.get('https://hashito.biz/')
 print(driver.find_element_by_xpath('/html/body/div[1]/div/section/div/div/h2').text)
-
-
 print(driver.current_url)
 driver.quit()
